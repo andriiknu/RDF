1471fba2bc4fbb6d1c99097a18a3d49b3d0c61a0
Andrii Falko <afalko@lxplus711.cern.ch>
2022-09-02 15:49:58 +0200

    bug with random numers solved

--- helper.cpp
+++ helper.cpp
@@ -35,8 +35,12 @@ ROOT::VecOps::RVec<float> pt_res_up(const ROOT::VecOps::RVec<float>& jet_pt)
 {
     // normal distribution with 5% variations, shape matches jets
     ROOT::VecOps::RVec<float> res(jet_pt.size());
-    TRandom2 rnmd;
-    for (auto& e: res) {rnmd.SetSeed(0); e = rnmd.Gaus(1,0.05);}
+//     TRandom2 rnmd;
+    for (auto& e: res) {
+//         rnmd.SetSeed(0); 
+//         e = rnmd.Gaus(1,0.05);
+        e = gRandom->Gaus(1,0.05);
+    }
     return res;
 }
 
